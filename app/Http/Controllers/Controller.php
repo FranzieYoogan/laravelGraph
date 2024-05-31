68429d3a7af36747778c2dbac8259cdad225ef40
Franzie Yoogan <franzieyoogan@gmail.com>
2024-05-30 22:59:37 -0300

    graph

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,34 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 
-abstract class Controller
+class Controller
 {
-    //
+    
+    public function sendEmotions(request $request) {
+
+        $happiness = $request->input('happiness');
+        $motivation = $request->input('motivation');
+        $determination = $request->input('determination');
+        $sadness = $request->input('sadness');
+        $fear = $request->input('fear');
+
+        if($happiness ||$motivation || $determination || $sadness || $fear) {
+
+            return view('welcome', ['happiness' => $happiness, 'motivation' => $motivation, 'determination' => $determination, 'sadness' => $sadness, 'fear' => $fear,]);
+
+        } else {
+
+            $happiness = 0;
+            $motivation = 0;
+            $determination = 0;
+            $sadness = 0;
+            $fear = 0;
+            return view('welcome', ['happiness' => $happiness, 'motivation' => $motivation, 'determination' => $determination, 'sadness' => $sadness, 'fear' => $fear,]);
+        }
+
+       
+    }
+
 }
